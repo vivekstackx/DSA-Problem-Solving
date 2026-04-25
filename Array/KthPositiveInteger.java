3d75d9d8597b31ed09ca2d279c76790ae9cbafeb
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-04-25 14:22:01 +0530

    Create  KthPositiveInteger.java

---
+++ Array/KthPositiveInteger.java
@@ -0,0 +1,36 @@
+
+class Solution {
+
+    // function to get Kth missing number
+    int getKthMissing(int[] arr, int KthNo) {
+        int start = 0;
+        int end = arr.length - 1;
+        int index = end + 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int missingNo = arr[mid] - (mid + 1);
+
+         if (missingNo >= KthNo) {
+                index = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return index + KthNo;
+    }
+}
+
+public class KthPositiveInteger {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        int[] arr = new int[] { 2, 3, 7, 11 };
+        int KthNo = 7;
+
+        int missingNo = obj.getKthMissing(arr, KthNo);
+        System.out.println(missingNo);
+    }
+}

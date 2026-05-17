1985fe865f702f1afa15d880157c6a2c8ae8a581
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-05-17 16:26:42 +0530

    Update NextGreaterElementM3.java

--- Stack/NextGreaterElementM3.java
+++ Stack/NextGreaterElementM3.java
@@ -9,7 +9,7 @@ class Solution{
            Stack<Integer> stack = new Stack<>();
            
           for(int i = arr.length - 1; i >= 0; i--){
-              while(!stack.isEmpty() && arr[stack.peek()] < arr[i]){
+              while(!stack.isEmpty() && arr[stack.peek()] <= arr[i]){
                 stack.pop();
               }
               if(!stack.isEmpty()){

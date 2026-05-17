c1e3859c091b59d3126541637b15744b5aec5d07
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-05-17 16:20:08 +0530

    Add PreviousSmallerElementM3 functionality
    Implemented a method to find previous smaller elements in an array and added a print function.

---
+++ Stack/PreviousSmallerElementM3.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+class Solution{
+     // function to find the previous smaller element in the array
+      int[] getNext(int[] arr){
+          int[] ans = new int[arr.length];
+          Arrays.fill(ans , -1);
+        Stack<Integer> stack = new Stack<>();
+        
+         for(int i = arr.length - 1; i >= 0; i--){
+            
+             while(!stack.isEmpty() && arr[i] < arr[stack.peek()]){
+               ans[stack.pop()] = arr[i];
+             }
+              stack.push(i);
+         }
+         
+          return ans;
+         
+   
+      }
+       
+      // print Array
+      void printArray(int[] arr){
+         for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " "); 
+         }
+         System.out.println();
+      }
+}
+
+
+
+public class PreviousSmallerElementM3{
+     public static void main(String[] args) {
+        int[] arr = {8, 6, 4, 7, 4, 9, 10, 8, 12};
+          
+        Solution obj = new Solution();
+        
+        obj.printArray(arr); 
+        
+        int[] ans = obj.getNext(arr);
+        
+        obj.printArray(ans);
+        
+          
+      
+     }
+}

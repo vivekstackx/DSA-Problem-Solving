de9d1cc5ce3de229f86ea6602b1cf2a4d8f4e843
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-05-17 16:17:17 +0530

    Create  NextSmallerElementM3.java

---
+++ Stack/NextSmallerElementM3.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+class Solution{
+     // function to find the next smaller element in the array
+      int[] getNext(int[] arr){
+          int[] ans = new int[arr.length];
+          Arrays.fill(ans , -1);
+          Stack<Integer> stack = new Stack<>();
+          
+          for(int i = arr.length - 1; i >= 0; i--){
+              
+              while(!stack.isEmpty() && arr[stack.peek()] >= arr[i]){
+                stack.pop();
+              }
+              if(!stack.isEmpty()){
+                 ans[i] = arr[stack.peek()];
+              }
+              stack.push(i);
+          }
+          return ans;
+         
+   
+      }
+       
+      // print Array
+      void printArray(int[] arr){
+         for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " "); 
+         }
+         System.out.println();
+      }
+}
+
+
+
+public class NextSmallerElementM3{
+     public static void main(String[] args) {
+        int[] arr = {8, 6, 4, 7, 4, 9, 10, 8, 12};
+          
+        Solution obj = new Solution();
+        
+        obj.printArray(arr); 
+        
+        int[] ans = obj.getNext(arr);
+        
+        obj.printArray(ans);
+        
+          
+      
+     }
+}

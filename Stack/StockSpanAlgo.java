5009c5a69e1e821a03ccb7f77b0895f64ae9b600
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-05-18 13:43:11 +0530

    Create StockSpanAlgo.java

---
+++ Stack/StockSpanAlgo.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+class Solution{
+   // stock span problem 
+    int[] stockSpan(int[] prices){
+         Stack<Integer> stack = new Stack<>();
+         int[] ans = new int[prices.length];
+         
+        for(int i = 0; i < prices.length; i++){
+            
+             while(!stack.isEmpty() && prices[stack.peek()] <= prices[i]){
+                stack.pop();
+             }
+             if(stack.isEmpty()){ // Greater element not found
+                ans[i] = i + 1;
+             }
+             else{ // Greater element found
+               ans[i] = i - stack.peek();
+             }
+             stack.push(i);
+        }
+        return ans;
+    }
+    void printArray(int[] arr){
+       for(int i = 0; i < arr.length; i++){
+          System.out.print(arr[i] + " ");
+       }
+       System.out.println();
+    }
+}
+
+public class StockSpanAlgo {
+    public static void main(String[] args) {
+       int[] prices = {3, 5, 10, 20, 52, 100, 80, 55, 70, 60, 75, 85};
+       
+       Solution obj = new Solution();
+        
+        obj.printArray(prices);
+        
+       int[] ans = obj.stockSpan(prices);
+       
+       obj.printArray(ans);
+       
+       
+    }
+}

6f3d85c192c4bda2ebc4a8ffab4c143be80272d2
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-05-29 22:15:16 +0530

    Update MaximumOfMinEveryWindowSizeM2.java

--- Stack/MaximumOfMinEveryWindowSizeM2.java
+++ Stack/MaximumOfMinEveryWindowSizeM2.java
@@ -52,3 +52,68 @@ public class MaximumOfMinEveryWindowSizeM2{
       obj.dis(ans);
     }
 }
+
+
+
+
+/*
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+
+    ArrayList<Integer> maxOfMins(int[] arr){
+
+        int n = arr.length;
+
+        ArrayList<Integer> ans =
+            new ArrayList<>(Collections.nCopies(n, Integer.MIN_VALUE));
+
+        for(int i = 0; i < n; i++){
+
+            int windowPosition = 0;
+            int min = Integer.MAX_VALUE;
+
+            for(int j = i; j < n; j++){
+
+                min = Math.min(min, arr[j]);
+
+                ans.set(
+                    windowPosition,
+                    Math.max(ans.get(windowPosition), min)
+                );
+
+                windowPosition++;
+            }
+        }
+
+        return ans;
+    }
+
+    void dis(ArrayList<Integer> arr){
+
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+
+        System.out.println();
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 15, 50, 10, 70, 30};
+
+        Solution obj = new Solution();
+
+        ArrayList<Integer> ans = obj.maxOfMins(arr);
+
+        obj.dis(ans);
+    }
+}
+
+
+*/

d16a98e691094c3b4c6f9c9e91505d98f482a7ae
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-06-08 17:25:07 +0530

    Create  GetFirstNoRepeatingCharacter.java

---
+++ Queue/GetFirstNoRepeatingCharacter.java
@@ -0,0 +1,50 @@
+class Solution{
+    // funciton to check a character is dublicate or not 
+     private boolean isRepeate(String stream , char ch , int index){
+        int n = stream.length();
+
+        for(int i = 0; i < n; i++){
+            if(stream.charAt(i) == ch && i != index){
+              return true;
+            }
+        }
+        return false;
+     }
+   // function to get no repeating character in stream
+   private char getNoRepeatingStream(String stream){
+        int n = stream.length();
+
+        for(int i = 0; i < n; i++){
+            if(!isRepeate(stream , stream.charAt(i) , i)){
+              return stream.charAt(i);
+            }
+        }
+        return '#';
+    }
+    
+    // function to get the first no repeating character in stream 
+       String getFirstNoRepeatingChar(String str){
+          
+           int n = str.length();
+           String stream = "";
+           String ans = "";
+
+           for(int i = 0; i < n; i++){
+                stream = stream + str.charAt(i);
+                char ch = getNoRepeatingStream(stream);
+                ans = ans + ch;
+           }
+           return ans;
+       }
+}
+
+public class GetFirstNoRepeatingCharacter {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        String str = "abcacdbd";
+
+        String ans = obj.getFirstNoReaptingChar(str);
+        System.out.println(ans);
+    }
+}

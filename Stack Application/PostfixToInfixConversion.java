ba5e5971423ba164badbb2b9d5de0f1cf06a1af7
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-06-18 17:10:02 +0530

    Create  PostfixToInfixConversion.java

---
+++ Stack Application/PostfixToInfixConversion.java
@@ -0,0 +1,79 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+class Solution {
+
+    // Check whether current symbol is an operator
+    private boolean isOperator(char symbol) {
+        return symbol == '+' || symbol == '-' ||
+               symbol == '*' || symbol == '/' ||
+               symbol == '^';
+    }
+
+    // Convert postfix expression to infix expression
+    String postfixToInfix(String postfix) {
+
+        // Stack stores intermediate infix expressions
+        Deque<String> stack = new ArrayDeque<>();
+
+        // Traverse each symbol of postfix expression
+        for (char symbol : postfix.toCharArray()) {
+
+            // If current symbol is an operator
+            if (isOperator(symbol)) {
+
+                // First popped operand becomes right operand
+                String first = stack.pop();
+
+                // Second popped operand becomes left operand
+                String second = stack.pop();
+
+                // Build a new infix expression and push back to stack
+                switch (symbol) {
+
+                    case '+':
+                        stack.push("(" + second + "+" + first + ")");
+                        break;
+
+                    case '-':
+                        stack.push("(" + second + "-" + first + ")");
+                        break;
+
+                    case '*':
+                        stack.push("(" + second + "*" + first + ")");
+                        break;
+
+                    case '/':
+                        stack.push("(" + second + "/" + first + ")");
+                        break;
+
+                    case '^':
+                        stack.push("(" + second + "^" + first + ")");
+                        break;
+                }
+            }
+            else {
+                // Operand directly goes into stack
+                stack.push(String.valueOf(symbol));
+            }
+        }
+
+        // Final infix expression will be present at top of stack
+        return stack.pop();
+    }
+}
+
+public class PostfixToInfixConversion{
+
+    public static void main(String[] args) {
+
+        // Sample postfix expression
+        String postfix = "ABC*D/+E-";
+
+        // Convert postfix to infix
+        String infix = new Solution().postfixToInfix(postfix);
+
+        // Print final infix expression
+        System.out.println(infix);
+    }
+}

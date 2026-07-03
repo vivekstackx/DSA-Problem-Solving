6e2c9a0c7a4b27ab11cf63ae0eb54bec2bbb2f36
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-07-03 19:24:49 +0530

    Create CycleDectectionLinkedList.java

---
+++ LinkedList/CycleDectectionLinkedList.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+class Node{
+   int data;
+   Node next;
+    Node(int data){
+      this.data = data;
+      this.next = null;
+    }
+}
+
+class Solution {
+  
+private boolean isVisited(ArrayList<Node> list , Node curr){
+
+   for(Node node : list){
+      if(node == curr){
+        return true;
+      }
+   }
+   return false;
+}
+
+   boolean isCycle(Node head){
+
+       Node curr = head;
+       ArrayList<Node> list = new ArrayList<>();
+
+       while(curr != null){
+         
+           if(isVisited(list , curr)){
+              return true; // cycle detetected.
+           }
+           list.add(curr);
+           curr = curr.next;
+       }
+
+       return false; // cycle not found !
+   }
+}
+
+
+class Main{
+   public static void main(String[] args){
+     Solution list = new Solution();
+
+     Node head = null;
+
+     head = new Node(1);
+     head.next = new Node(2);
+     head.next.next = new Node(3);
+     head.next.next.next = new Node(4);
+     head.next.next.next.next = new Node(5);
+     head.next.next.next.next.next = new Node(6);
+     head.next.next.next.next.next.next = new Node(4);
+     head.next.next.next.next.next.next.next = new Node(8);
+     head.next.next.next.next.next.next.next.next = new Node(9);
+     head.next.next.next.next.next.next.next.next.next = new Node(10);
+     head.next.next.next.next.next.next.next.next.next.next = head.next.next.next;
+
+
+     System.out.println(list.isCycle(head));
+
+
+    
+    
+   }
+}

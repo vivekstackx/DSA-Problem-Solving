bd7c6e520fd4aed8b0fbc76c72c6d71dd94bbe17
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-07-07 14:08:37 +0530

    Update getStartingNodeOfCycle.java

--- LinkedList/getStartingNodeOfCycle.java
+++ LinkedList/getStartingNodeOfCycle.java
@@ -1,29 +1,69 @@
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
+class Node{
+   int data;
+   Node next;
+
+     Node(int data){
+       this.data = data;
+       this.next = null;
+     }
+}
+
+class Solution{
+   
+     Node getBeginingNodeOfCyle(Node head){
         
-         ListNode slowPointer = head;
-         ListNode fastPointer = head;
+          Node slow = head;
+          Node fast = head;
 
-         while(fastPointer != null && fastPointer.next != null){
-            slowPointer = slowPointer.next;
-            fastPointer = fastPointer.next.next;
+          while(fast != null && fast.next != null){
 
-            if(slowPointer == fastPointer){
-                break;
-            }
+              slow = slow.next;
+              fast = fast.next.next;
+
+              if(slow == fast){
+                 break; // cycle found
+              }
+          }
+         
+         if(fast == null || fast.next == null){
+           return null; // no cycle
          }
 
-         if(fastPointer == null || fastPointer.next == null){
-            return null; // no cycle 
+         slow = head;
+         while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
          }
+         
+         return slow;
+     }
+}
 
-         slowPointer = head;
+public class Main {
+    public static void main(String[] args) {
+      
+       Node head = null;
+       head = new Node(1);
+       head.next = new Node(2);
+       head.next.next = new Node(3);
+       head.next.next.next = new Node(4);
+       head.next.next.next.next = new Node(5);
+       head.next.next.next.next.next = new Node(6);
+       head.next.next.next.next.next.next = new Node(7);
+       head.next.next.next.next.next.next.next = new Node(8);
+       head.next.next.next.next.next.next.next.next = new Node(9);
+       head.next.next.next.next.next.next.next.next.next = new Node(10);
+       head.next.next.next.next.next.next.next.next.next.next = new Node(11);
+       head.next.next.next.next.next.next.next.next.next.next.next = new Node(12);
+       head.next.next.next.next.next.next.next.next.next.next.next =  head.next.next.next;
 
-         while(slowPointer != fastPointer){
-            slowPointer = slowPointer.next;
-            fastPointer = fastPointer.next;
-         }
 
-         return slowPointer;
+       
+       Node startNode = new Solution().getBeginingNodeOfCyle(head);
+       System.out.println(startNode.data);
+
+
+       
+
     }
 }

27eb82663a2d8e58c85bdf3cb57ba24d1ec627d9
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-07-07 14:59:22 +0530

    Create GetCycleNodeM2.java

---
+++ LinkedList/GetCycleNodeM2.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+
+class Node{
+   int data;
+   Node next;
+
+     Node(int data){
+       this.data = data;
+       this.next = null;
+     }
+}
+
+class Solution{
+    
+    // get total cycle node 
+     ArrayList<Integer> getCycleNode(Node head){
+        
+          // find the start node of cycle 
+          Node slow = head;
+          Node fast = head;
+
+          while(fast != null && fast.next != null){
+
+              slow = slow.next;
+              fast = fast.next.next;
+
+              if(slow == fast){
+                break; // cycle found
+              }
+          }
+
+          if(fast == null || fast.next == null){
+            return new ArrayList<>(); // no cycle
+          }
+         
+          slow = head;
+          while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+          }
+          
+          ArrayList<Integer> cycleNodeList = new ArrayList<>();
+
+          do{
+             cycleNodeList.add(slow.data);
+             slow = slow.next;
+            }while(slow != fast);
+
+            return cycleNodeList;
+     }
+}
+
+public class Main {
+    public static void main(String[] args) {
+      
+       Node head = null;
+       head = new Node(1);
+       head.next = new Node(2);
+       head.next.next = new Node(3);
+       head.next.next.next = new Node(4);
+       head.next.next.next.next = new Node(5);
+       head.next.next.next.next.next = new Node(6);
+       head.next.next.next.next.next.next = new Node(7);
+       head.next.next.next.next.next.next.next = new Node(8);
+       head.next.next.next.next.next.next.next.next = new Node(9);
+       head.next.next.next.next.next.next.next.next.next = new Node(10);
+       head.next.next.next.next.next.next.next.next.next.next = new Node(11);
+       head.next.next.next.next.next.next.next.next.next.next.next = new Node(12);
+     head.next.next.next.next.next.next.next.next.next.next.next.next =  head.next.next.next;
+
+
+       
+       ArrayList<Integer> cycleNode = new Solution().getCycleNode(head);
+       System.out.println(cycleNode);
+
+
+       
+
+    }
+}

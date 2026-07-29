f71e02b1665d86c26e723e675b04daa3ed16dac8
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-07-29 18:31:28 +0530

    Create LeftViewOfBinaryTree.java

---
+++ Tree/LeftViewOfBinaryTree.java
@@ -0,0 +1,35 @@
+lass Solution {
+    public ArrayList<Integer> leftView(Node root) {
+        // code here
+         ArrayList<Integer> list = new ArrayList<>();
+         if(root == null){
+             return list;
+         }
+         Queue<Node> queue = new ArrayDeque<>();
+         queue.offer(root);
+         
+         while(!queue.isEmpty()){
+             // current level
+             
+              // store first node of each level in list
+             list.add(queue.peek().data);
+             
+             int size = queue.size();
+            
+             while(size > 0){
+                 
+                Node temp = queue.poll();
+                size--;
+                
+                if(temp.left != null){
+                    queue.offer(temp.left);
+                }
+                if(temp.right != null){
+                    queue.offer(temp.right);
+                }
+             }
+         }
+         
+         return list;
+    }
+}

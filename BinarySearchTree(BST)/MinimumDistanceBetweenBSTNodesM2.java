02c3aa570a97a942e1fc272f52c7d785f8c73c05
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-08-24 17:07:03 +0530

    Create MinimumDistanceBetweenBSTNodesM2.java

---
+++ BinarySearchTree(BST)/MinimumDistanceBetweenBSTNodesM2.java
@@ -0,0 +1,34 @@
+
+class Solution {
+    private int previousData;
+    private int minDiff;
+    private boolean hasPrevious;
+    
+    
+    private void inorderTraverse(TreeNode root){
+      
+        if(root == null) return;
+        
+        inorderTraverse(root.left);
+        
+         if(hasPrevious){
+             minDiff = Math.min(minDiff , root.val - previousData);
+         }
+         previousData = root.val;
+         hasPrevious = true;
+         
+        inorderTraverse(root.right);
+    }
+    
+
+    public int minDiffInBST(TreeNode root) {
+        
+       hasPrevious = false;
+        
+       minDiff = Integer.MAX_VALUE;
+       
+       inorderTraverse(root);
+       
+       return minDiff;
+    }
+}

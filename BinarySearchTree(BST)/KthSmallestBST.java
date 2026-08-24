d10fd9e359a48db3233a6d3709be728ed35a8033
Vivek Kumar <194188560+vivekstackx@users.noreply.github.com>
2026-08-24 17:44:08 +0530

    Create KthSmallestBST.java

---
+++ BinarySearchTree(BST)/KthSmallestBST.java
@@ -0,0 +1,37 @@
+
+class Solution {
+
+  private void inorderTraverse(TreeNode root , List<Integer> inorderList){
+        
+         if(root == null) return;
+         
+         inorderTraverse(root.left , inorderList);
+         
+         inorderList.add(root.val);
+         
+         inorderTraverse(root.right , inorderList);
+    }
+
+
+    public int kthSmallest(TreeNode root, int k) {
+
+        List<Integer> inorderList = new ArrayList<>();
+        
+        inorderTraverse(root, inorderList);
+
+        int kth = Integer.MAX_VALUE;
+
+        for(int i = 0; i < inorderList.size(); i++){
+            
+            if(k == 1){
+              kth = inorderList.get(i);
+              break;
+            }
+
+            k--;
+        }
+        
+        return kth;
+
+    }
+}
